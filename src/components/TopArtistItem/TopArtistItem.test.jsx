5fe7ccf269cb23e1a64b23f6d1a1118dcb6e523a
YannisMOUCHARD <yannis.mouchard@etu.univ-poitiers.fr>
2025-11-13 13:24:15 +0100

    fix:bug top artist

--- src/components/TopArtistItem/TopArtistItem.test.jsx
+++ src/components/TopArtistItem/TopArtistItem.test.jsx
@@ -1,3 +1,4 @@
+
 // src/components/PlayListItem.test.jsx
 
 import { describe, expect, test } from '@jest/globals'
@@ -12,7 +13,7 @@ describe('TopArtistItem component', () => {
             name: 'Test Artist',
             images: [{ url: 'test.jpg' }, { url: 'test-medium.jpg' }, { url: 'test-small.jpg' }],
             genres: ['pop', 'rock'],
-            followers: { total: 1000 },
+            followers: { total: 100 },
             popularity: 85,
             external_urls: { spotify: 'https://open.spotify.com/artist/artist1' }
         };
