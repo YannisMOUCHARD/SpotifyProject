0d4b5d60982e5e7c740d2da1bf56d22a4c5c0887
YannisMOUCHARD <github-action@actions-template-sync.noreply.github.com>
2025-11-17 16:46:20 +0000

    chore(template): merge template changes :up:
    Signed-off-by: YannisMOUCHARD <github-action@actions-template-sync.noreply.github.com>

--- src/components/TopArtistItem/TopArtistItem.test.jsx
+++ src/components/TopArtistItem/TopArtistItem.test.jsx
@@ -1,4 +1,3 @@
-
 // src/components/PlayListItem.test.jsx
 
 import { describe, expect, test } from '@jest/globals'
@@ -13,7 +12,7 @@ describe('TopArtistItem component', () => {
             name: 'Test Artist',
             images: [{ url: 'test.jpg' }, { url: 'test-medium.jpg' }, { url: 'test-small.jpg' }],
             genres: ['pop', 'rock'],
-            followers: { total: 100 },
+            followers: { total: 1000 },
             popularity: 85,
             external_urls: { spotify: 'https://open.spotify.com/artist/artist1' }
         };
